54d34cae20914082ba00fa5f2a6e01d6a67aeaea
Joy Lin <vvlnote@hotmail.com>
2019-10-01 23:37:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,3 @@
-CREATE TABLE projects (
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT,
-funding_goal INTEGER,
-start_date TEXT,
-end_date TEXT
-);
 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "A", "sing_family", 300000, 2019-08-20, 2020-09-11);
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "B", "sing_family", 250000, 2019-07-25, 2020-10-10);
